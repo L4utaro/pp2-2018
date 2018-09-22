a7ef568a6e2b004407497e9ef2699eaa117caaea
Lautaro <lautydb@yahoo.com>
2018-09-22 10:17:38 -0300

    exe

---
+++ p/src/main/java/executors/ExecutorMove.java
@@ -0,0 +1,17 @@
+package executors;
+
+import model.Map;
+
+public class ExecutorMove implements IExecutor {
+	private Map map;
+
+	public ExecutorMove(Map map) {
+		this.map = map;
+	}
+
+	public void makeCommandAction() {
+		//this.map.getAvatarPosition;
+
+	}
+
+}
